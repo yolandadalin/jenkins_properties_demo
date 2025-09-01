a3970fa6c67ebae211855483bf5eae93eced10b5
yolandadalin <yolandalin6021@gmail.com>
2025-09-01 18:12:14 +0800

    Fix double SpringBootServletInitializer Error

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -2,16 +2,15 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.example.hello.config.EnvSupport;
 
 @SpringBootApplication
-public class HelloApplication extends SpringBootServletInitializer {
+public class HelloApplication {
 
 	public static void main(String[] args) {
     var app = new SpringApplication(HelloApplication.class);
-    app.setAdditionalProfiles(EnvSupport.resolveEnv()); // 內建 Tomcat 跑時也啟用對應 profile
+    app.setAdditionalProfiles(EnvSupport.resolveEnv()); // 內建 Tomcat 一致
     app.run(args);
   }
 
