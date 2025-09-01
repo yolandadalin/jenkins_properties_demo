eafc7d9ec4a4236f8744994ff458d611364b6b27
yolandadalin <yolandalin6021@gmail.com>
2025-09-01 18:00:26 +0800

    Modify HelloApplication

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -1,15 +1,18 @@
 package com.example.hello;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.example.hello.config.EnvSupport;
 
 @SpringBootApplication
 public class HelloApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		var app = new org.springframework.boot.SpringApplication(HelloApplication.class);
-        app.setAdditionalProfiles(com.example.hello.config.EnvSupport.resolveEnv());
-        app.run(args);
-	}
+    var app = new SpringApplication(HelloApplication.class);
+    app.setAdditionalProfiles(EnvSupport.resolveEnv()); // 內建 Tomcat 跑時也啟用對應 profile
+    app.run(args);
+  }
 
 }
